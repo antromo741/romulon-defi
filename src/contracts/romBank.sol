709f0b5f720db62af897f218524a84279b8d665d
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-07-20 12:33:31 -0400

    added the rombank contract and the skelleton for what I will fill in

---
+++ src/contracts/romBank.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+import "./Token.sol";
+
+contract dBank {
+
+ 
+  constructor() public {
+   
+  }
+
+  function deposit() payable public {
+ 
+  }
+
+  function withdraw() public {
+ 
+  }
+
+  function borrow() payable public {
+
+  }
+
+  function payOff() public {
+   
+  }
+}
