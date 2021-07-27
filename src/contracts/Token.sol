2e6ca56497c4335e125ff3557cae8bd1cb2a66a0
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-07-27 15:45:47 -0400

    added the passminter role, which will allow the minter to be the person who deployed the application

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -6,16 +6,22 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Token is ERC20 {
   address public minter;
 
-  //add minterchanged event
+ 
 
   constructor() public payable ERC20("Decentralized Bank Currency", "DCB") {
     minter = msg.sender;
   }
 
-  //Add pass minter role function
+  function passMinterROle(address dbank) public returns(bool) {
+    require(msg.semder==minter, 'Error, onlu owner can change pass minter role');
+    minter = dbank;
+
+    emit MinterChanged(msg.semder, dbank);
+    return true;
+  }
 
   function mint(address account, uint256 amount) public {
-    require(msg.sender == minter, 'Error, msg.sender does not have the minter role');
+    require(msg.sender==minter, 'Error, msg.sender does not have the minter role');
 		_mint(account, amount);
 	}
 }
