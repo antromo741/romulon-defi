13bdd266cfab48b36006019ffa10a72d29dfe5c2
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-08-03 11:45:38 -0400

    added an interest rate to allow people to earn tokens on while the ether is deposited

--- src/contracts/romBank.sol
+++ src/contracts/romBank.sol
@@ -37,14 +37,22 @@ contract romBank {
   }
 
   function withdraw() public {
-    
+    require(isDeposited[msg.sender] == true, 'Error, no previous deposit');
+
     //check users hold time
     uint depositTime = block.timestamp - depositStart[msg.sender];
     
+
+    uint interestPerSecond = 31668017 * (etherBalanceOf[msg.sender] / 1e16);
+    uint interest = interesPerSecond * depositTime;
     //sends eth to user
     msg.sender.transfer(etherBalanceOf[msg.sender]);
+    token.mint(msg.sender, interest);
+
     //reset depositer data
+    depositStart[msg.sender] = 0;
     etherBalanceOf[msg.sender] = 0;
+    isDeposited[msg.sender] = false;
   }
 
   function borrow() payable public {
