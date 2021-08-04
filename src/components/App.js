efd8dfd5c8e39fcf407f83de1eeef01110f11c6a
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-08-04 17:22:44 -0400

    styling was not working cause pretext in  app.css, added payoff and loan tabs with backend connections, now must update the contracts

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
-import { Tabs, Tab } from 'react-bootstrap';
+import { Tabs, Tab } from 'react-bootstrap'
+import romBank from '../abis/romBank.json'
 import React, { Component } from 'react';
-import romBank from '../abis/romBank.json';
-import Token from '../abis/Token.json'  ;
+import Token from '../abis/Token.json'
 import rombank from '../romBank.png';
 import Web3 from 'web3';
 import './App.css';
 
-
-//Add loan options to token
 class App extends Component {
 
   async componentWillMount() {
@@ -15,47 +13,39 @@ class App extends Component {
   }
 
   async loadBlockchainData(dispatch) {
-
-    //check if MetaMask exists
-    if(typeof window.ethereum!=='undefined'){
-
+    if (typeof window.ethereum !== 'undefined') {
       const web3 = new Web3(window.ethereum)
       const netId = await web3.eth.net.getId()
       const accounts = await web3.eth.getAccounts()
 
-      if(typeof accounts[0] !=='undefined'){
+      //load balance
+      if (typeof accounts[0] !== 'undefined') {
         const balance = await web3.eth.getBalance(accounts[0])
-        this.setState({ account: accounts[0], balance: balance, web3: web3})
+        this.setState({ account: accounts[0], balance: balance, web3: web3 })
       } else {
-        window.alert('Please login wih MetaMask')
+        window.alert('Please login with MetaMask')
       }
-      try{
-      const token = new web3.eth.Contract(Token.abi, Token.networks[netId].address)
-      const rombank = new web3.eth.Contract(romBank.abi, romBank.networks[netId].address)
-      const romBankAddress = romBank.networks[netId].address
 
-      //Also need to add tab to see interest
-      //Need to elaborate on this part to figure out interest"
-      const tokenBalance = await token.methods.balanceOf(this.state.account).call()
-      console.log(web3.utils.fromWei(tokenBalance))
-      this.setState({token: token, romBank: romBank, romBankAddress: romBankAddress})
+      //load contracts
+      try {
+        const token = new web3.eth.Contract(Token.abi, Token.networks[netId].address)
+        const rombank = new web3.eth.Contract(romBank.abi, romBank.networks[netId].address)
+        const romBankAddress = romBank.networks[netId].address
+        this.setState({ token: token, rombank: rombank, romBankAddress: romBankAddress })
       } catch (e) {
         console.log('Error', e)
         window.alert('Contracts not deployed to the current network')
       }
 
     } else {
-      window.alert('Please install MetaMask, Import an account from Ganache and add the Ganache private Network')
+      window.alert('Please install MetaMask')
     }
-   
   }
 
   async deposit(amount) {
-    console.log(amount)
-      if(this.state.rombank!=='undefined'){
-        try{
-      
-      await this.state.rombank.methods.deposit().send({value: amount.toString(), from: this.state.account})
+    if (this.state.rombank !== 'undefined') {
+      try {
+        await this.state.rombank.methods.deposit().send({ value: amount.toString(), from: this.state.account })
       } catch (e) {
         console.log('Error, deposit: ', e)
       }
@@ -63,12 +53,36 @@ class App extends Component {
   }
 
   async withdraw(e) {
+    e.preventDefault()
     if (this.state.rombank !== 'undefined') {
       try {
-
         await this.state.rombank.methods.withdraw().send({ from: this.state.account })
       } catch (e) {
         console.log('Error, withdraw: ', e)
+      }
+    }
+  }
+
+  async borrow(amount) {
+    if (this.state.rombank !== 'undefined') {
+      try {
+        await this.state.rombank.methods.borrow().send({ value: amount.toString(), from: this.state.account })
+      } catch (e) {
+        console.log('Error, borrow: ', e)
+      }
+    }
+  }
+
+  async payOff(e) {
+    e.preventDefault()
+    if (this.state.rombank !== 'undefined') {
+      try {
+        const collateralEther = await this.state.rombank.methods.collateralEther(this.state.account).call({ from: this.state.account })
+        const tokenBorrowed = collateralEther / 2
+        await this.state.token.methods.approve(this.state.romBankAddress, tokenBorrowed.toString()).send({ from: this.state.account })
+        await this.state.rombank.methods.payOff().send({ from: this.state.account })
+      } catch (e) {
+        console.log('Error, pay off: ', e)
       }
     }
   }
@@ -96,12 +110,12 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             <img src={rombank} className="App-logo" alt="logo" height="32" />
-            <b>romBank</b>
+            <b>rom₿ank</b>
           </a>
         </nav>
         <div className="container-fluid mt-5 text-center">
           <br></br>
-          <h1>Welcome to romBank</h1>
+          <h1>Welcome to rom₿ank</h1>
           <h2>{this.state.account}</h2>
           <br></br>
           <div className="row">
@@ -120,35 +134,76 @@ class App extends Component {
                       <form onSubmit={(e) => {
                         e.preventDefault()
                         let amount = this.depositAmount.value
-                        amount = amount * 10**18 //comeback and use toWei()
+                        amount = amount * 10 ** 18 //convert to wei
+                        this.deposit(amount)
                       }}>
                         <div className='form-group mr-sm-2'>
                           <br></br>
                           <input
-                              className='form-control form-control-md'
-                              id='depositAmount'
-                              placeholder='Amount?'
-                              step="0.01"
-                              type='number'
-                              required
-                              ref={(input) => { this.depositAmount = input }}
-                          />
+                            id='depositAmount'
+                            step="0.01"
+                            type='number'
+                            ref={(input) => { this.depositAmount = input }}
+                            className="form-control form-control-md"
+                            placeholder='amount...'
+                            required />
                         </div>
-                        <button type ='submit' className='btn btn-primary'>Deposit</button>
+                        <button type='submit' className='btn btn-primary'>DEPOSIT</button>
                       </form>
+
                     </div>
                   </Tab>
                   <Tab eventKey="withdraw" title="Withdraw">
+                    <br></br>
+                    Do you want to withdraw + take interest?
+                    <br></br>
+                    <br></br>
                     <div>
+                      <button type='submit' className='btn btn-primary' onClick={(e) => this.withdraw(e)}>WITHDRAW</button>
+                    </div>
+                  </Tab>
+                  <Tab eventKey="borrow" title="Borrow">
+                    <div>
+
                       <br></br>
-                      Do you want to withdraw everything and take interest?
+                      Do you want to borrow tokens?
+                      <br></br>
+                      (You'll get 50% of collateral, in Tokens)
+                      <br></br>
+                      Type collateral amount (in ETH)
                       <br></br>
                       <br></br>
-                      <div>
-                        <button type='submit' className='btn btn-primary' onClick={(e) => this.withdraw(e)}>
-                        WITHDRAW
-                        </button>
-                      </div>
+                      <form onSubmit={(e) => {
+
+                        e.preventDefault()
+                        let amount = this.borrowAmount.value
+                        amount = amount * 10 ** 18 //convert to wei
+                        this.borrow(amount)
+                      }}>
+                        <div className='form-group mr-sm-2'>
+                          <input
+                            id='borrowAmount'
+                            step="0.01"
+                            type='number'
+                            ref={(input) => { this.borrowAmount = input }}
+                            className="form-control form-control-md"
+                            placeholder='amount...'
+                            required />
+                        </div>
+                        <button type='submit' className='btn btn-primary'>BORROW</button>
+                      </form>
+                    </div>
+                  </Tab>
+                  <Tab eventKey="payOff" title="Payoff">
+                    <div>
+
+                      <br></br>
+                      Do you want to payoff the loan?
+                      <br></br>
+                      (You'll receive your collateral - fee)
+                      <br></br>
+                      <br></br>
+                      <button type='submit' className='btn btn-primary' onClick={(e) => this.payOff(e)}>PAYOFF</button>
                     </div>
                   </Tab>
                 </Tabs>
